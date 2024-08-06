87f29cb3bba05ae553a6cac57f335b3f653c71bf
Jeryl Goh <jerylgoh2003@gmail.com>
2024-08-06 21:27:13 +0800

    Created Shopping Item Composable
    created the UI composable for each shopping item that will appear on the UI when a new shopping item is added

--- app/src/main/java/com/example/myshoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/example/myshoppinglistapp/ShoppingList.kt
@@ -1,5 +1,7 @@
 package com.example.myshoppinglistapp
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,6 +10,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem(val id: Int,
@@ -55,7 +60,7 @@ fun ShoppingListApp()
                 .padding(16.dp))
         {
             items(shoppingItems){
-
+                ShoppingListItem(item = it, {}, {})
             }
         }
     }
@@ -118,6 +123,28 @@ fun ShoppingListApp()
             }
         )
 
+    }
+}
+
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick: () -> Unit, //a lambda function, we can pass a lambda function as a parameter into a function
+    onDeleteClick: () -> Unit,
+)
+{
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .border(
+                border = BorderStroke(2.dp, Color.Black),
+                shape = RoundedCornerShape(20)
+            )
+    ) {
+        Text(text = item.name, modifier = Modifier.padding(8.dp));
+        Text(text = item.quantity.toString(), modifier = Modifier.padding(8.dp))
     }
 }
 

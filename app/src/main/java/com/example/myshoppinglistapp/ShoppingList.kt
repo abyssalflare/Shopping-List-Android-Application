fc5590c2391b344ffe2657beb1bf5561ddbd6d35
Jeryl Goh <jerylgoh2003@gmail.com>
2024-08-06 15:14:38 +0800

    Added AlertDialog
    coded alertDialog base to act as the pop up when we add an item to the shoppinglist. Will modify alertdialog to work in our favor

--- app/src/main/java/com/example/myshoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/example/myshoppinglistapp/ShoppingList.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -25,17 +27,20 @@ data class ShoppingItem(val id: Int,
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShoppingListApp()
 {
-    var shoppingItems by remember{ mutableStateOf(listOf<ShoppingItem>()) }
+    //mutable states
+    var shoppingItems by remember{ mutableStateOf(listOf<ShoppingItem>())};
+    var showDialog by remember{ mutableStateOf(false)};
 
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
     ){
         Button(
-            onClick = { /*TODO*/ },
+            onClick = {showDialog = true},
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
             Text(text = "Add Item");
@@ -48,6 +53,13 @@ fun ShoppingListApp()
             items(shoppingItems){
 
             }
+        }
+    }
+
+    if(showDialog)
+    {
+        AlertDialog(onDismissRequest = {showDialog = false}) {
+            Text(text = "THIS IS THE LAPD!");
         }
     }
 }
